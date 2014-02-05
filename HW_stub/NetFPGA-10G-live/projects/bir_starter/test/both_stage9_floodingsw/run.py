64c61153831c9835394a7d4e0e14a75dd9418aae
baz21 <baz@cam.ac.uk>
2014-02-05 15:27:45 +0000

    Add hardware tests for the two ports and dma (on two ports) as well; they pass.

--- HW_stub/NetFPGA-10G-live/projects/bir_starter/test/both_stage9_floodingsw/run.py
+++ HW_stub/NetFPGA-10G-live/projects/bir_starter/test/both_stage9_floodingsw/run.py
@@ -31,6 +31,52 @@ for i in range(4):
     #pkt.tuser_sport = 1 << (i * 2);
     pkts.append(pkt)
 
+if isHW() and 1:
+    for i in range(send_n_packets):
+	    nftest_send_phy('nf0', pkts[0])
+	    nftest_expect_phy('nf1', pkts[0])
+	    nftest_barrier()
+    for i in range(send_n_packets):
+	    nftest_send_phy('nf1', pkts[1])
+	    nftest_expect_phy('nf0', pkts[1])
+	    nftest_barrier()
+
+if isHW() and 1:
+    for i in range(send_n_packets):
+	    nftest_send_dma('nf0', pkts[0])
+	    nftest_expect_phy('nf0', pkts[0])
+	    nftest_expect_phy('nf1', pkts[0])
+	    nftest_barrier()
+    for i in range(send_n_packets):
+	    nftest_send_dma('nf1', pkts[1])
+	    nftest_expect_phy('nf0', pkts[1])
+	    nftest_expect_phy('nf1', pkts[1])
+	    nftest_barrier()
+    # Seems if there's no physical port in the connections file
+    # the test framework does not like dma on it either:
+    #
+    #	Running test using the following physical connections:
+    #	nf1:eth3
+    #	nf0:eth2
+    #	------------------------------------------------------
+    #	Traceback (most recent call last):
+    #	  File "/tmp/root/test/bir_starter/both_stage9_floodingsw/run.py", line 60, in <module>
+    #	    nftest_send_dma('nf2', pkts[2])
+    #	  File "/root/netfpga/HW_stub/NetFPGA-10G-live/lib/python/NFTest/NFTestLib.py", line 223, in nftest_send_dma
+    #	    sent_dma[ifaceName].append(pkt)
+    #	KeyError: 'nf2'
+    if 0:
+	    for i in range(send_n_packets):
+		    nftest_send_dma('nf2', pkts[2])
+		    nftest_expect_phy('nf0', pkts[2])
+		    nftest_expect_phy('nf1', pkts[2])
+		    nftest_barrier()
+	    for i in range(send_n_packets):
+		    nftest_send_dma('nf3', pkts[3])
+		    nftest_expect_phy('nf0', pkts[3])
+		    nftest_expect_phy('nf1', pkts[3])
+		    nftest_barrier()
+
 if not isHW() and 1:
     for i in range(send_n_packets):
         xpkts.append(pkts[0])
