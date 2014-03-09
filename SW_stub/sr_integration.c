78145de70f255ff588c15b927caf24bb907d9dd1
Charlie Bashford <charlietbc@gmail.com>
2014-03-09 08:39:05 -0700

    set ip filters

--- SW_stub/sr_integration.c
+++ SW_stub/sr_integration.c
@@ -59,12 +59,12 @@ void sr_integ_hw_setup( struct sr_instance* sr ) {
     uint32_t *ip = malloc(sizeof(uint32_t));
     unsigned i;
     for (i = 0; i < router->num_interfaces; i++) {
-        writeReg(router->nf.fd, XPAR_NF10_ROUTER_OUTPUT_PORT_LOOKUP_0_FILTER_IP, ntohs(router->interface[i].ip));
         writeReg(router->nf.fd, XPAR_NF10_ROUTER_OUTPUT_PORT_LOOKUP_0_FILTER_WR_ADDR, i);
-        
+        writeReg(router->nf.fd, XPAR_NF10_ROUTER_OUTPUT_PORT_LOOKUP_0_FILTER_IP, ntohs(router->interface[i].ip));
         
         writeReg(router->nf.fd, XPAR_NF10_ROUTER_OUTPUT_PORT_LOOKUP_0_FILTER_RD_ADDR, i);
         readReg(router->nf.fd, XPAR_NF10_ROUTER_OUTPUT_PORT_LOOKUP_0_FILTER_IP, ip);
+        
         debug_println("ip=%04X, read_ip=%04X", router->interface[i].ip, ip);
         assert(*ip == ntohs(router->interface[i].ip));
     }
